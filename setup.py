6569c107dfb9a3201ca9598c477fc087ef554447
Thom Linton <tlinton@pdx.edu>
2024-09-23 15:28:12 -0700

    Updates Python ecosystem controls, configuration.
    - Adds support for PEP 517, PEP 518, PEP 621, PEP 660.
    - Uses Pipenv to manage dependencies.

--- setup.py
+++ setup.py
@@ -1,42 +1,7 @@
-import os
-from setuptools import setup, find_packages
-
-
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+# -*- coding: utf-8 -*-
+from setuptools import setup
 
 
 setup(
-    name='django-envelope',
-    version=__import__('envelope').__version__,
-    description='A contact form app for Django',
-    long_description=read('README.rst'),
-    author='Zbigniew Siciarz',
-    author_email='zbigniew@siciarz.net',
-    url='http://github.com/zsiciarz/django-envelope',
-    download_url='http://pypi.python.org/pypi/django-envelope',
-    license='MIT',
-    install_requires=['Django>=1.11'],
-    packages=find_packages(exclude=['example_project', 'tests']),
-    include_package_data=True,
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Web Environment',
-        'Framework :: Django',
-        'Framework :: Django :: 1.11',
-        'Framework :: Django :: 2.2',
-        'Framework :: Django :: 3.0',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Topic :: Utilities',
-    ],
+    setup_requires=['setuptools_scm'],
 )
